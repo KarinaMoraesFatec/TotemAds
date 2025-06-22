c31345ac4c05f70d84e429a8bd46db2a33d6b0c4
Karina Moraes Fatec <karianaua1@gmail.com>
2025-06-22 17:37:22 -0300

    add negado ok

--- TotemPWA-main/Controllers/HomeController.cs
+++ TotemPWA-main/Controllers/HomeController.cs
@@ -67,7 +67,12 @@ public class HomeController : Controller
     {
     return View();
     }
-    
+
+     public IActionResult Negado()
+    {
+    return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
